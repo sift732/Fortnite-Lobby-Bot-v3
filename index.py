96883d498d5579108438751a60c318c37269bc7d
darkguide <83960086+sift732@users.noreply.github.com>
2026-05-20 17:41:43 +0900

    Update

--- index.py
+++ index.py
@@ -1,21 +1,12 @@
 import json
 import asyncio
 
-from rebootpy import Client
-
-from modules.auth import MyAdvancedAuth
-from modules import translate
-from utils import color
-from utils.logger import Logger
+from modules.bot import run_bots
 from modules.update import Updater
 
-color.init()
-translate.load_lang()
 
 with open("config.json", "r", encoding="utf-8") as f:
     config = json.load(f)
-
-client = Client(auth=MyAdvancedAuth())
 
 
 async def startup_update():
@@ -25,23 +16,10 @@ async def startup_update():
     updater = Updater()
     await updater.run()
 
-@client.event
-async def event_device_code_generated(link):
-    Logger.info(
-        translate.get("main.confirm_authorization", link)
-    )
-
-
-@client.event
-async def event_ready():
-    Logger.success(
-        translate.get("client.ready", client.user)
-    )
-
 
 def start():
     asyncio.run(startup_update())
-    client.run()
+    run_bots(config)
 
 
 if __name__ == "__main__":
